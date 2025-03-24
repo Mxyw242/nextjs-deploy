a546a031605f501ed7977fc451076226aed2ade7
Kulthira Thongduang <mameawmumi@gmail.com>
2025-03-24 14:14:25 +0700

    add api create attractions

--- app/api/attractions/route.js
+++ app/api/attractions/route.js
@@ -9,7 +9,21 @@ export async function GET(request) {
       `SELECT * FROM attractions;`
     )
     return NextResponse.json(rows)
-  } catch(error) {
-    return NextResponse.json({error:"Failed to fetch"}, {status: 500})
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to fetch" }, { status: 500 })
+  }
+}
+
+export async function POST(request) {
+  try{
+    const { name, detail, coverimage, latitude, longitude } = await request.json();
+    const [result] = await db.query(
+      'INSERT INTO attractions(name, detail, coverimage, latitude, longitude)VALUES (?, ?, ?, ?, ?)'
+      , [name, detail, coverimage, latitude, longitude]
+    );
+    return NextResponse.json({ id:result.insertId, name, detail, coverimage, latitude, longitude }
+      , { status: 200 });
+  }catch(error){
+    return NextResponse.json({ error: error }, { status: 500 })
   }
 }
